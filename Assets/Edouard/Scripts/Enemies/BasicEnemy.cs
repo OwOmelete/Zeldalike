6a9e1cebd6c8e4007bacc1bea56248dedb4a0b87
EdouardRubika <laffont.edu@gmail.com>
2026-03-31 10:26:35 -0400

    Basic Enemy Optimised

---
+++ Assets/Edouard/Scripts/Enemies/BasicEnemy.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    #region Stats
+
+    [Header("Stats")]
+    [SerializeField] private int maxHealthPoints;
+    [SerializeField] private int currentHealthPoints;
+    
+    [SerializeField] private int damagePoints;
+    
+    [SerializeField] private int moveSpeed;
+
+    #endregion
+
+    #region StateFlags
+
+    public enum StateFlags
+    {
+        IDLE,
+        CHASING,
+        ATTACKING,
+    }
+    [Header("StateFlags")]
+    [SerializeField] private  StateFlags currentStateFlag;
+
+    #endregion
+    
+    [Header("Other")]
+    [SerializeField] private GameObject detectorGameObject;
+
+    private Transform playerTransform;
+    
+    private void Start()
+    {
+        currentHealthPoints = maxHealthPoints;
+        HandleEnemyState(StateFlags.IDLE);
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        switch (currentStateFlag)
+        {
+            case StateFlags.IDLE:
+                IdleBehavior();
+                break;
+            case StateFlags.CHASING:
+                ChasingBehavior();
+                break;
+            case StateFlags.ATTACKING:
+                AttackingBehavior();
+                break;
+        }
+    }
+
+    public void HandleEnemyState(StateFlags state)
+    {
+        if (currentStateFlag == state) return;
+        
+        currentStateFlag = state;
+        Debug.Log($"Changed state to {state}");
+    }
+
+    private void IdleBehavior()
+    {
+        Debug.Log("Current behaviour is idle");
+    }
+
+    private void ChasingBehavior()
+    {
+        Debug.Log("Current behaviour is chasing");
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position,
+            playerTransform.position, moveSpeed * Time.deltaTime);
+        gameObject.transform.LookAt(playerTransform);
+    }
+    private void AttackingBehavior()
+    {
+        Debug.Log("Current behaviour is attacking");
+    }
+    
+    
+}
