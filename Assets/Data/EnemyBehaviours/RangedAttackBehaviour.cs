edf30920f18f41e3ff444383ff33220db025efa8
Edouard Laptop <laffont.edu@gmail.com>
2026-04-25 15:50:26 -0400

    Crée EnemyProjectile (script) + EnemyRangedAttackBehaviour (script)

---
+++ Assets/Data/EnemyBehaviours/RangedAttackBehaviour.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemies/Behaviours/RangedAttack")]
+public class RangedAttackBehaviour : EnemyBehaviourSO
+{
+    public GameObject projectilePrefab;
+    public Transform firePoint;
+    public float cooldown = 1.5f;
+
+    private float lastAttackTime;
+
+    public override void Execute(BasicEnemyV3 enemy)
+    {
+        if (enemy.Player == null) return;
+        if (enemy.IsAttacking) return;
+
+        if (Time.time < lastAttackTime + cooldown) return;
+
+        enemy.StartCoroutine(Fire(enemy));
+        lastAttackTime = Time.time;
+    }
+
+    private IEnumerator Fire(BasicEnemyV3 enemy)
+    {
+        enemy.IsAttacking = true;
+
+        GameObject proj = Object.Instantiate(
+            projectilePrefab,
+            firePoint.position,
+            Quaternion.identity
+        );
+
+        EnemyProjectile p = proj.GetComponent<EnemyProjectile>();
+
+        var playerHealth = enemy.Player.GetComponent<PlayerHealthComponent>();
+
+        p.Init(enemy.Player, playerHealth);
+
+        yield return new WaitForSeconds(0.3f);
+
+        enemy.IsAttacking = false;
+    }
+}
