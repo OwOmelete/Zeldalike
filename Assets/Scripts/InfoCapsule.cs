4aace39d849682f8257f45940780cf9aeb43fc8c
unknown <f.lefebvre@RUBIKA.CA.LOCAL>
2026-04-30 16:43:28 -0400

    Level 2 Infotext UPDATE

--- Assets/Scripts/InfoCapsule.cs
+++ Assets/Scripts/InfoCapsule.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class InfoCapsule : MonoBehaviour
 {
     public TextMeshProUGUI infoCanva;
-
+    
+    [TextArea(3, 10)]
     public string infoText;
     
     public void OnTriggerEnter(Collider collision)
