630ee95160e713f9f4a8a41e402f2b21750db5ee
ZERTHEXS <h.zengers@rubika-edu.com>
2026-05-19 14:22:28 -0400

    Prefab Source famillier parent a mettre dans la beta :)

---
+++ Assets/3D/LV2/Tuyaux.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Tuyaux : MonoBehaviour
+{
+    [SerializeField] private Material baseMaterial;
+    [SerializeField] private Material instanceMaterial;
+    public float vitesseDePropagation;
+    float ratio ;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   void Start()
+    {
+        ratio=vitesseDePropagation;
+        instanceMaterial = new Material(baseMaterial);
+        GetComponent<Renderer>().material = instanceMaterial;
+        instanceMaterial.SetFloat("_Ice_Progression",1);
+
+         StartCoroutine(TurnOnPipe());
+    }
+     IEnumerator TurnOnPipe()
+    {
+        
+        while (vitesseDePropagation > -7)
+        {
+            vitesseDePropagation-=Time.deltaTime;
+            instanceMaterial.SetFloat("_Ice_Progression",vitesseDePropagation/ratio);
+            yield return null;
+        }
+        yield return null;
+    }
+}
