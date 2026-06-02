4a4585ba2c6d9c421d2a4b3e7693f6a7b2b9c87e
ZERTHEXS <h.zengers@rubika-edu.com>
2026-06-02 11:43:03 -0400

    FX impact

--- Assets/Sprites/placeholder/AnimationUI.cs
+++ Assets/Sprites/placeholder/AnimationUI.cs
@@ -5,26 +5,30 @@ public class AnimationUI : MonoBehaviour
     public Animator Left;
     public Animator Right;
     public bool Switch;
-    public InputActionReference LeftTrigger;
-    public InputActionReference RightTrigger;
+    public Material material;
+    public Color32 colorFroid;
+    public Color32 colorChaud;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Switch = false;
+        material.SetColor("_EmissionColor",colorFroid);
     }
     void Update()
     {
-        if(Gamepad.current.leftTrigger.ReadValue() > 0.2f) OnLeftTrigger();
-        if(Gamepad.current.rightTrigger.ReadValue() > 0.2f) OnRightTrigger();
+        if(Gamepad.current.leftShoulder.wasPressedThisFrame && !Switch) OnLeftTrigger();
+        if(Gamepad.current.rightShoulder.wasPressedThisFrame && Switch) OnRightTrigger();
     }
 
     private void OnLeftTrigger()
     {
-        
+
             Left.SetTrigger("Close");
             Right.SetTrigger("Open");
             Switch=true;
+            material.SetColor("_EmissionColor",colorChaud);
+            
     }
 
     private void OnRightTrigger()
@@ -32,6 +36,7 @@ public class AnimationUI : MonoBehaviour
         Left.SetTrigger("Open");
         Right.SetTrigger("Close");
         Switch=false;
+        material.SetColor("_EmissionColor",colorFroid);
     }
     
 }
