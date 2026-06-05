2ed0d2b3567ef5427b924e819e02b06236979829
ZERTHEXS <h.zengers@rubika-edu.com>
2026-06-04 20:10:13 -0400

    TowerDefence

--- Assets/Sprites/UI/TowerDefenseGame/TourInvokFamillier.cs
+++ Assets/Sprites/UI/TowerDefenseGame/TourInvokFamillier.cs
@@ -7,6 +7,7 @@ using Unity.Mathematics;
 public class TourInvokFamillier : MonoBehaviour
 {
 [SerializeField] SplineContainer zoneSpawn;
+public GameObject origine;
 [SerializeField] List<GameObject> unite = new List<GameObject>();
 int uniteCount;
     void Start()
@@ -35,6 +36,8 @@ int uniteCount;
                 zoneSpawn.Evaluate(randomValue, out float3 position, out float3 tangent, out float3 upVector);
                 Vector3 SpawnPoint = zoneSpawn.transform.TransformPoint(position)/2;
                 unite[uniteCount].transform.position = SpawnPoint;
+                Vector3 dir = origine.transform.position - SpawnPoint;
+                unite[uniteCount].transform.position -= dir*0.8f;
                 uniteCount++;
                 yield return new WaitForSeconds(10f); 
             }
